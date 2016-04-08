56cf2188a9f16fac0a0e833f236519f7153925b1
User <levangersundet@Mainframe2.levangernett.no>
2016-04-08 18:22:22 +0200

    rename

--- django/testproject/app_urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import *  # NOQA
-from testproject.views import TestCreateView
-
-urlpatterns = patterns('',
-    url(r'^$', TestCreateView.as_view()),
-    url(r'^(?P<testtype>baat|datakort|sol)/$', TestCreateView.as_view())
-)
