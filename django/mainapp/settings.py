ba2358158b69354461cba64f8432e9f31db7692f
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-10-22 17:11:43 +0200

    first attempt

--- django/mainapp/settings.py
+++ django/mainapp/settings.py
@@ -67,28 +67,10 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 SITE_ID = 1
-
-if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
-
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    AWS_S3_HOST = 's3-%s.amazonaws.com' % os.environ['AWS_REGION']
-
-    #AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    #AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-
-    STATICFILES_LOCATION = 'static'
-    MEDIAFILES_LOCATION = 'media'
-
-    STATICFILES_STORAGE = 'mainapp.custom_storages.StaticStorage'
-    DEFAULT_FILE_STORAGE = 'mainapp.custom_storages.MediaStorage'
-
-    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-else:
-    STATIC_URL = '/static/'
-    MEDIA_URL = '/media/'
 
 TEMPLATES = [
     {
@@ -208,17 +190,6 @@ DATABASES = {
         'NAME': os.path.join(DATA_DIR, 'django.db'),
     }
 }
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_DB_USERNAME'],
-            'PASSWORD': os.environ['RDS_DB_PASSWORD'],
-            'HOST': os.environ['RDS_DB_HOST'],
-            'PORT': os.environ['RDS_DB_PORT'],
-        }
-    }
 
 MIGRATION_MODULES = {
     'cmsplugin_filer_image': 'cmsplugin_filer_image.migrations_django',
